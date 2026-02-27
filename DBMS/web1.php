f12a73a63fb6d4106cab1ac1e0164ea494c36936
Arman Hossen <2armanhossen@gmail.com>
2026-02-27 12:46:26 +0600

    web1

---
+++ DBMS/web1.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>DBMS-WEB1</title>
+</head>
+<body>
+    <h2>Data Collect From Website to Mysql</h2>
+
+    <form action="">
+        Name     : <input type="text">
+        <br>
+        Email    : <input type="text">
+        <br>
+        Password : <input type="password">
+        <br>
+        <input type="submit">
+        <br>
+    </form>
+
+    
+</body>
+</html>
